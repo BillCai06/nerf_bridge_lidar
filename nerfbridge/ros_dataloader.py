a6c52ba32622b61bf5ed3743dbfefd733681c5c0
KamalanathanN <kamal.rkara@gmail.com>
2024-02-25 16:57:15 -0500

    trains need to fix image frame not loading and pose spacing

--- nerfbridge/ros_dataloader.py
+++ nerfbridge/ros_dataloader.py
@@ -14,6 +14,7 @@ from typing import Union
 
 from rich.console import Console
 import torch
+import numpy as np
 from torchvision.transforms import Resize
 from torch.utils.data.dataloader import DataLoader
 
@@ -22,18 +23,47 @@ from nerfbridge.ros_dataset import ROSDataset, ROSDepthDataset
 
 import rclpy
 from sensor_msgs.msg import Image, CompressedImage
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseArray
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
 from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer, Subscriber
 from cv_bridge import CvBridge
 
+import sensor_msgs_py.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2
+from nerfstudio.process_data.colmap_utils import qvec2rotmat
+import scipy.spatial.transform as transform
+from scipy.spatial.transform import Rotation 
+
+
+
 CONSOLE = Console(width=120)
 
-# Suppress a warning from torch.tensorbuffer about copying that
+# Suppress a warning from torch.tensorbuffer about copyhttps://github.com/BillCai06/nerf_bridge_lidar.giting that
 # does not apply in this case.
 warnings.filterwarnings("ignore", "The given buffer")
 
+def ros_pose_to_homogenous(pose_message: PoseStamped):
+    """
+    Converts a ROS2 Pose message to a 4x4 homogenous transformation matrix
+    as a torch tensor (half precision).
+    """
+    quat = pose_message.pose.orientation
+    pose = pose_message.pose.position
+
+    R = Rotation.from_quat([quat.x, quat.y, quat.z, quat.w]).as_matrix()
+    t = torch.Tensor([pose.x, pose.y, pose.z])
+
+    T = torch.eye(4)
+    T[:3, :3] = torch.from_numpy(R)
+    T[:3, 3] = t
+    return T.to(dtype=torch.float32)
+
+def ros_pose_to_nerfstudio(pose: PoseStamped, static_transform=None):
+    hom_pose =ros_pose_to_homogenous(pose)
+    T_ns = hom_pose[:, [1, 2, 0, 3]]
+    T_ns[:, [0, 2]] *= -1
+    return T_ns[:3, :]
 
 class ROSDataloader(DataLoader):
     """
@@ -72,11 +102,11 @@ class ROSDataloader(DataLoader):
         topic_slop: float,
         use_compressed_rgb: bool,
         device: Union[torch.device, str] = "cpu",
+        publish_posearray: bool = True,
         **kwargs,
     ):
         # This is mostly a parameter placeholder, and manages the cameras
         self.dataset = dataset
-
         # Image meta data
         self.device = device
         self.num_images = len(self.dataset)
@@ -89,8 +119,15 @@ class ROSDataloader(DataLoader):
         self.updated = True
         self.update_period = 1 / data_update_freq
         self.last_update_t = time.perf_counter()
+        self.publish_posearray = publish_posearray
         self.poselist = []
 
+        self.coord_st = torch.zeros(3, 4)
+        R1 = transform.Rotation.from_euler("x", 180, degrees=True).as_matrix()
+        R2 = transform.Rotation.from_euler("z", 0, degrees=True).as_matrix()
+        R = torch.from_numpy(R2 @ R1)
+        self.coord_st[:, :3] = R
+        
         # Keep it in the format so that it makes it look more like a
         # regular data loader.
         self.data_dict = {
@@ -98,13 +135,13 @@ class ROSDataloader(DataLoader):
             "image_idx": self.dataset.image_indices,
         }
 
-        # Flag for depth training, add depth tensor to data.
-        self.listen_depth = False
-        if isinstance(self.dataset, ROSDepthDataset):
-            self.data_dict["depth_image"] = self.dataset.depth_tensor
-            # For resizing depth image to match color image.
-            self.depth_transform = Resize((self.H, self.W))
-            self.listen_depth = True
+        # # Flag for depth training, add depth tensor to data.
+        # self.listen_depth = False
+        # if isinstance(self.dataset, ROSDepthDataset):
+        #     self.data_dict["depth_image"] = self.dataset.depth_tensor
+        #     # For resizing depth image to match color image.
+        #     self.depth_transform = Resize((self.H, self.W))
+        #     self.listen_depth = True
 
         super().__init__(dataset=dataset, **kwargs)
 
@@ -153,11 +190,15 @@ class ROSDataloader(DataLoader):
             raise NameError(
                 "Unsupported SLAM algorithm. Must be one of {cuvslam, orbslam3}"
             )
+        
+        self.subs.append(
+            Subscriber(self.node, PointCloud2, self.dataset.lidar_topic_name)
+        )
 
-        if self.listen_depth:
-            self.subs.append(
-                Subscriber(self.node, Image, self.dataset.depth_topic_name)
-            )
+        # if self.listen_depth:
+        #     self.subs.append(
+        #         Subscriber(self.node, Image, self.dataset.depth_topic_name)
+        #     )
 
         if topic_sync == "approx":
             self.ts = ApproximateTimeSynchronizer(self.subs, 40, topic_slop)
@@ -169,6 +210,7 @@ class ROSDataloader(DataLoader):
             )
 
         self.ts.registerCallback(self.ts_callback)
+        self.posearray_pub = self.node.create_publisher(PoseArray, 'training_poses', 1)
 
         # Start a thread for processing the callbacks
         self.ros_thread = threading.Thread(
@@ -183,92 +225,184 @@ class ROSDataloader(DataLoader):
         """
         return self.current_idx >= (num_to_start - 1)
 
-    def ts_callback(self, *args):
+    def ts_callback(self, image: Image, pose: PoseStamped, cloud_msg: PointCloud2):
+        """
+        The callback triggered when time synchronized image and pose messages
+        are published on the topics specifed in the config JSON passed to
+        the ROSDataParser.
+        """
+        # print("-------------------------------started---------------")
         now = time.perf_counter()
         if (
             now - self.last_update_t > self.update_period
             and self.current_idx < self.num_images
         ):
-            # Process RGB and Pose
-            self.image_callback(args[0])
-            self.pose_callback(args[1])
-
-            # Process Depth if using depth training
-            if self.listen_depth:
-                self.depth_callback(args[2])
-
-            self.health_pub.publish(
-                String(
-                    data=f"Num Train Imgs {self.current_idx:04}, dT={now - self.last_update_t:0.4f}"
-                )
+            # ----------------- Handling the IMAGE ----------------
+            # Load the image message directly into the torch
+            # print("-------------image index: ",self.current_idx,"----------------------")
+            im_tensor = torch.frombuffer(image.data, dtype=torch.uint8).reshape(
+                self.H, self.W, -1
             )
+            im_tensor = im_tensor.to(dtype=torch.float32) / 255.0
+            # Convert BGR -> RGB (this adds an extra copy, and might be able to
+            # skip if we do something fancy with the reshape above)
+           
+            # im_tensor = im_tensor.flip([-1])
 
+            # COPY the image data into the data tensor
+            im_tensor_rgb = im_tensor[:, :, :3]  # Keep only the first 3 channels (RGB)
+            im_tensor_rgb = im_tensor_rgb[:, :, [2, 1, 0]]  # Reorder channels from BGR to RGB
+            # im_tensor_rgb = im_tensor[:, :, [2, 1, 0]]  # Rearrange the channels
+
+            # image_np = im_tensor_rgb.cpu().detach().numpy()
+            # plt.imshow(image_np)
+            # plt.axis('off')  # Turn off axis numbers and ticks
+            # plt.show()
+            self.dataset.image_tensor[self.current_idx] = im_tensor_rgb
+
+            # ----------------- Handling the POSE ----------------
+            c2w = ros_pose_to_nerfstudio(pose, static_transform=self.coord_st)
+            device = self.dataset.cameras.device
+            c2w = c2w.to(device)
+            self.dataset.cameras.camera_to_worlds[self.current_idx] = c2w
+
+            if self.publish_posearray:
+                self.poselist.append(pose.pose)
+                pa = PoseArray(poses=self.poselist)
+                pa.header.frame_id = "map"
+                self.posearray_pub.publish(pa)
+            # print("-------------image pose: ",self.poselist,"----------------------")
             self.dataset.updated_indices.append(self.current_idx)
+
+            #-------------------------------Handling Lidar------------------------------
+            cloud_points = list(pc2.read_points(cloud_msg, field_names=("x", "y", "z", "rgb"), skip_nans=True))
+        
+            # Separate XYZ coordinates and RGB colors
+            xyz = [(x, y, z) for x, y, z, _ in cloud_points]
+            rgbs = [rgb for _, _, _, rgb in cloud_points]
+            
+            # Convert RGB floats to integer values, then unpack them into separate R, G, B channels
+            rgb_ints = np.array(rgbs, dtype=np.uint32)
+            r = ((rgb_ints >> 16) & 0xFF).astype(np.int64)  # Convert to np.int64
+            g = ((rgb_ints >> 8) & 0xFF).astype(np.int64)   # Convert to np.int64
+            b = (rgb_ints & 0xFF).astype(np.int64)          # Convert to np.int64
+            
+            # Convert XYZ and RGB data into PyTorch tensors for further processing
+            xyz_tensor = torch.tensor(xyz, dtype=torch.float32)
+            rgb_tensor = torch.stack((
+                torch.tensor(r, dtype=torch.float32),
+                torch.tensor(g, dtype=torch.float32),
+                torch.tensor(b, dtype=torch.float32)
+            ), dim=1)
+            device = self.dataset.cameras.device
+            xyzrgb_tensor = torch.cat((xyz_tensor, rgb_tensor / 255.0), dim=1)  # Normalize RGB to [0, 1]
+            xyzrgb_tensor = xyzrgb_tensor.to(device)
+            self.dataset.update_point_cloud(self.current_idx, xyzrgb_tensor)
+            # print("saved Lidar points")
+            #---------------------------- end of lidar process------------------------------------
             self.updated = True
             self.current_idx += 1
             self.last_update_t = now
 
-    def image_callback(self, image: Image | CompressedImage):
-        """
-        Callback for processing RGB Image Messages, and adding them to the
-        dataset for training.
-        """
-        # Load the image message directly into the torch
-        if self.use_compressed_rgb:
-            im_cv = self.bridge.compressed_imgmsg_to_cv2(image)
-            im_tensor = torch.from_numpy(im_cv).to(dtype=torch.float32) / 255.0
-            im_tensor = torch.flip(im_tensor, [-1])
-        else:
-            im_cv = self.bridge.imgmsg_to_cv2(image, image.encoding)
-            im_tensor = torch.from_numpy(im_cv).to(dtype=torch.float32) / 255.0
+    # def lidar_callback(self, lidar_msg: pc2.PointCloud2):
+    #     cloud_points = list(pc2.read_points(lidar_msg, field_names=("x", "y", "z", "rgb"), skip_nans=True))
+    
+    #     # Separate XYZ coordinates and RGB colors
+    #     xyz = [(x, y, z) for x, y, z, _ in cloud_points]
+    #     rgbs = [rgb for _, _, _, rgb in cloud_points]
+        
+    #     # Convert RGB floats to integer values, then unpack them into separate R, G, B channels
+    #     rgb_ints = np.array(rgbs, dtype=np.uint32)
+    #     r = ((rgb_ints >> 16) & 0xFF).astype(np.int64)  # Convert to np.int64
+    #     g = ((rgb_ints >> 8) & 0xFF).astype(np.int64)   # Convert to np.int64
+    #     b = (rgb_ints & 0xFF).astype(np.int64)          # Convert to np.int64
+        
+    #     # Convert XYZ and RGB data into PyTorch tensors for further processing
+    #     xyz_tensor = torch.tensor(xyz, dtype=torch.float32)
+    #     rgb_tensor = torch.stack((
+    #         torch.tensor(r, dtype=torch.float32),
+    #         torch.tensor(g, dtype=torch.float32),
+    #         torch.tensor(b, dtype=torch.float32)
+    #     ), dim=1)
+    #     device = self.dataset.cameras.device
+    #     xyzrgb_tensor = torch.cat((xyz_tensor, rgb_tensor / 255.0), dim=1)  # Normalize RGB to [0, 1]
+    #     xyzrgb_tensor = xyzrgb_tensor.to(device)
+    #     self.dataset.update_point_cloud(self.current_idx, xyzrgb_tensor)
 
+    # def image_callback(self, image: Image | CompressedImage):
+    #     """
+    #     Callback for processing RGB Image Messages, and adding them to the
+    #     dataset for training.
+    #     """
+    #     # Load the image message directly into the torch
+    #     if self.use_compressed_rgb:
+    #         im_cv = self.bridge.compressed_imgmsg_to_cv2(image)
+    #         im_tensor = torch.from_numpy(im_cv).to(dtype=torch.float32) / 255.0
+    #         im_tensor = torch.flip(im_tensor, [-1])
+    #     else:
+    #         im_cv = self.bridge.imgmsg_to_cv2(image, image.encoding)
+    #         im_tensor = torch.from_numpy(im_cv).to(dtype=torch.float32) / 255.0
 
-        # COPY the image data into the data tensor
-        self.dataset.image_tensor[self.current_idx] = im_tensor
+    #     # COPY the image data into the data tensor
+    #     im_tensor_rgb = im_tensor[:, :, :3]  # Keep only the first 3 channels (RGB)
+    #     im_tensor_rgb = im_tensor_rgb[:, :, [2, 1, 0]]  # Reorder channels from BGR to RGB
+    #     # im_tensor_rgb = im_tensor[:, :, [2, 1, 0]]  # Rearrange the channels
 
-    def pose_callback(self, pose: PoseStamped | Odometry):
-        """
-        Callback for Pose messages. Extracts pose, converts it to Nerfstudio coordinate
-        convention, and inserts it into the Cameras object.
-        """
-        if self.slam_method == "cuvslam":
-            # Odometry Message
-            hom_pose = pose_utils.ros_pose_to_homogenous(pose.pose)
-            c2w = pose_utils.cuvslam_to_nerfstudio(hom_pose)
-        elif self.slam_method == "orbslam3":
-            # PoseStamped Message
-            hom_pose = pose_utils.ros_pose_to_homogenous(pose)
-            c2w = pose_utils.orbslam3_to_nerfstudio(hom_pose)
-        elif self.slam_method == "mocap":
-            # PoseStamped Message
-            hom_pose = pose_utils.ros_pose_to_homogenous(pose)
-            c2w = pose_utils.mocap_to_nerfstudio(hom_pose)
-        else:
-            raise NameError("Unknown SLAM Method!")
+    #     self.dataset.image_tensor[self.current_idx] = im_tensor_rgb
 
-        # Scale Pose to
-        c2w[:3, 3] *= self.dataset.scale_factor
+    # def pose_callback(self, pose: PoseStamped | Odometry):
+    #     """
+    #     Callback for Pose messages. Extracts pose, converts it to Nerfstudio coordinate
+    #     convention, and inserts it into the Cameras object.
+    #     """
+    #     if self.slam_method == "cuvslam":
+    #         # Odometry Message
+    #         hom_pose = pose_utils.ros_pose_to_homogenous(pose.pose)
+    #         c2w = pose_utils.cuvslam_to_nerfstudio(hom_pose)
+    #     elif self.slam_method == "orbslam3":
+    #         # PoseStamped Message
+    #         hom_pose = pose_utils.ros_pose_to_homogenous(pose)
+    #         c2w = pose_utils.orbslam3_to_nerfstudio(hom_pose)
+    #     elif self.slam_method == "mocap":
+    #         # PoseStamped Message
+    #         hom_pose = pose_utils.ros_pose_to_homogenous(pose)
+    #         c2w = pose_utils.mocap_to_nerfstudio(hom_pose)
+    #         self.publish_posearray=True
+    #     else:
+    #         raise NameError("Unknown SLAM Method!")
 
-        # Insert in Cameras
-        device = self.dataset.cameras.device
-        self.dataset.cameras.camera_to_worlds[self.current_idx] = c2w.to(device)
+    #     # Scale Pose to
+    #     c2w[:3, 3] *= self.dataset.scale_factor
 
-    def depth_callback(self, depth: Image):
-        """
-        Callback for processing Depth Image messages. Similar to RGB image handling,
-        but also rescales the depth to the appropriate value.
-        """
-        depth_cv = self.bridge.imgmsg_to_cv2(depth, depth.encoding)
-        depth_tensor = torch.from_numpy(depth_cv.astype("float32")).to(
-            dtype=torch.float32
-        )
+    #     # Insert in Cameras
+    #     device = self.dataset.cameras.device
+    #     self.dataset.cameras.camera_to_worlds[self.current_idx] = c2w.to(device)
 
-        # depth_resized = self.depth_transform(depth_tensor)
-        aggregate_scale = self.dataset.scale_factor * self.dataset.depth_scale_factor
+    #     if self.publish_posearray:
+    #         self.poselist.append(pose.pose)
+    #         pa = PoseArray(poses=self.poselist)
+    #         pa.header.frame_id = "map"
+    #         self.posearray_pub.publish(pa)
+    #     # print("-------------image pose: ",self.poselist,"----------------------")
+    #     self.dataset.updated_indices.append(self.current_idx)
 
-        self.dataset.depth_tensor[self.current_idx] = (
-            depth_tensor.unsqueeze(-1) * aggregate_scale
-        )
+    # def depth_callback(self, depth: Image):
+    #     """
+    #     Callback for processing Depth Image messages. Similar to RGB image handling,
+    #     but also rescales the depth to the appropriate value.
+    #     """
+    #     depth_cv = self.bridge.imgmsg_to_cv2(depth, depth.encoding)
+    #     depth_tensor = torch.from_numpy(depth_cv.astype("float32")).to(
+    #         dtype=torch.float32
+    #     )
+
+    #     aggregate_scale = (
+    #         self.dataset.scene_scale_factor * self.dataset.depth_scale_factor
+    #     )
+
+    #     self.dataset.depth_tensor[self.current_idx] = (
+    #         depth_tensor.unsqueeze(-1) * aggregate_scale
+    #     )
 
     def __getitem__(self, idx):
         return self.dataset.__getitem__(idx)
--- nerfbridge/ros_dataset.py
+++ nerfbridge/ros_dataset.py
@@ -42,6 +42,7 @@ class ROSDataset(InputDataset):
         assert self.num_images > 0
         self.image_height = self.metadata["image_height"]
         self.image_width = self.metadata["image_width"]
+        self.scene_scale_factor = dataparser_outputs.dataparser_scale 
         self.device = device
 
         self.cameras = self.cameras.to(device=self.device)
@@ -51,14 +52,6 @@ class ROSDataset(InputDataset):
         )
         self.image_indices = torch.arange(self.num_images)
 
-        if "depth_topic_name" in self.metadata.keys():
-            self.depth_topic_name = self.metadata["depth_topic_name"]
-            self.depth_tensor = torch.ones(
-                self.num_images,
-                self.image_height,
-                self.image_width,
-                dtype=torch.float32,
-            )
         self.updated_indices = []
 
     def __len__(self):
@@ -121,3 +114,71 @@ class ROSDepthDataset(ROSDataset):
         data = super().__getitem__(idx)
         data["depth"] = self.depth_tensor[idx]
         return data
+
+class ROSLidarDataset(ROSDataset):
+    """
+    This is a tensor dataset that keeps track of all of the data streamed by ROS.
+    It's main purpose is to conform to the already defined workflow of nerfstudio:
+        (dataparser -> inputdataset -> dataloader).
+    """
+
+    def __init__(
+        self,
+        dataparser_outputs: DataparserOutputs,
+        scale_factor: float = 1.0,
+        device: Union[torch.device, str] = "cpu",
+    ):
+        super().__init__(dataparser_outputs, scale_factor, device)
+        assert (
+            "point_cloud_topic" in dataparser_outputs.metadata.keys()
+        )
+
+
+        self.lidar_topic_name = self.metadata["point_cloud_topic"]
+        self.max_points = 13000  # Example size, adjust based on your application's needs
+        self.xyzrgb_tensor = torch.ones(
+            self.num_images,
+            self.max_points,
+            6,
+            dtype=torch.float32,
+        )
+
+    def __getitem__(self, idx: int):
+        """
+        This returns the data as a dictionary which is not actually how it is
+        accessed in the dataloader, but we allow this as well so that we do not
+        have to rewrite the several downstream functions.
+        """
+        data = super().__getitem__(idx)
+        data["xyzrgb"] = self.xyzrgb_tensor[idx]
+        return data
+    
+    def update_point_cloud(self, idx: int, xyzrgb_data: torch.Tensor):
+        """
+        Update the stored XYZRGB data at the specified index, truncating or padding the data
+        as necessary to fit the pre-allocated tensor size.
+
+        Args:
+            idx: Index of the image/point cloud pair in the dataset.
+            xyzrgb_data: A tensor containing the XYZRGB data for the given index.
+        """
+     
+        if idx < self.num_images:
+          
+            # Determine whether to truncate or pad the xyzrgb_data
+            if xyzrgb_data.shape[0] > self.max_points:
+                # Truncate the xyzrgb_data if it has more points than max_points
+                truncated_data = xyzrgb_data[:self.max_points, :]
+                self.xyzrgb_tensor[idx] = truncated_data
+            elif xyzrgb_data.shape[0] < self.max_points:
+                # Pad the xyzrgb_data with zeros if it has fewer points than max_points
+                padded_data = torch.zeros((self.max_points, xyzrgb_data.shape[1]), dtype=xyzrgb_data.dtype, device=xyzrgb_data.device)
+                padded_data[:xyzrgb_data.shape[0], :] = xyzrgb_data
+                self.xyzrgb_tensor[idx] = padded_data
+                # print("Feed lidar data to idx with padding",idx," with shape:",self.xyzrgb_tensor[idx].shape[0])
+
+            else:
+                # Directly assign the data if it matches the expected size
+                self.xyzrgb_tensor[idx] = xyzrgb_data
+        else:
+            raise IndexError("Index out of bounds for the dataset.")
